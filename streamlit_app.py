af1c41b92cb74a1e8f65ca09460386f31704d878
shivuduj <141001927+shivuduj@users.noreply.github.com>
2024-08-21 22:23:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
         st.subheader(fruit_choosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
